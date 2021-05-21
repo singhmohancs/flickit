74f2bdafacc659cdad9563d6aad89cd9a54f46ff
Shivali Sharma <shivali@techlursoftware.com>
2021-05-21 14:36:42 +0530

    home js changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
                   <div class="col-md-6">
                      <div class="banner-text">
                         <h1 class="m-0"><span>We’re Making Insurance</span> Surprisingly
-                           Painless
+                           Painless.
                         </h1>
                         <p class="mb-0">Modern Insurance with Smart technology</p>
                         <ComingSoon styleClass="mt-60 btn-header btn btn-primary" />   
@@ -36,7 +36,7 @@ const Home = () => {
                         <img alt="phone-img-tablet" class="phone-img-tablet" src={phoneTablet} />
                         <img alt="phone-img-mob" class="phone-img-mob" src={phoneMobScreen} />
 
-                         {/* <div  class="banner-service-card top-left">
+                     {/* <div  class="banner-service-card top-left">
                            <img  alt="icon" src={require('../static/img/icon-smartphone.png').default} />
                            <h5>Smart Insurance</h5>
                         </div>
@@ -47,7 +47,8 @@ const Home = () => {
                         <div class="banner-service-card center-right">
                            <img alt="cash-icon" src={require('../static/img/icon-cash.png').default} />
                            <h5>Instant Quote</h5>
-                        </div> */}
+                        </div>  */}
+                       
                      </div>
                   </div>
                </div>
@@ -64,14 +65,14 @@ const Home = () => {
                   </div>
                   <div class="col-md-6">
                      <div class="flickit-content">
-                        <div class="flickit-heading"><span>Paperless </span>Experience</div>
+                        <div class="flickit-heading"><span>Paperless </span>Experience .</div>
                         <p>Seriously fast, Hassle free and flexible insurance with just a few
-                           clicks
+                           clicks.
                         </p>
                         <ul class="flickit-list list-unstyled">
                            <li>100% Paperless experience</li>
                            <li>Quote in Few Seconds</li>
-                           <li>No more lengthy applications and paperwork</li>
+                           <li>No more lengthy applications and paperwork .</li>
                         </ul>
                      </div>
                   </div>
@@ -90,11 +91,11 @@ const Home = () => {
                      <div class="flickit-content">
                         <div class="flickit-heading"><span>Purchase a cover </span> with a few
                            clicks, flexible
-                           payments, <span>Manage your policies</span> seamlessly
+                           payments, <span>Manage your policies</span> seamlessly.
                         </div>
                         <p>Purchasing an insurance policy was never this easy. Get your policy
                            in your inbox
-                           within a few minutes and manage your policies seamlessly
+                           within a few minutes and manage your policies seamlessly.
                         </p>
                      </div>
                   </div>
@@ -112,11 +113,11 @@ const Home = () => {
                   <div class="col-md-6">
                      <div class="flickit-content">
                         <div class="flickit-heading">File Claims <span>Instantly</span>, Fast
-                           claims experience
+                           claims experience.
                         </div>
                         <p>Lightning fast claims process. Our AI algorithms are always ready to
                            process claims
-                           speedily and promptly
+                           speedily and promptly.
                         </p>
                      </div>
                   </div>
@@ -135,7 +136,7 @@ const Home = () => {
                      <div class="flickit-content">
                         <div class="flickit-heading"><span>Pay bills easily.</span> View proof
                            of insurance
-                           anywhere
+                           anywhere.
                         </div>
                         <p>With our automated channels, you can make secure payments effortlessly and view your POI anywhere and at any time.
                         </p>
@@ -154,10 +155,10 @@ const Home = () => {
                      <div class="flickit-content centered">
                         <div class="flickit-heading"><span>Total transparency,</span> know
                            exactly what you’re
-                           paying for
+                           paying for.
                         </div>
                         <p>We are building trust through complete transparency across board. Insurance that is
-                           customer-centric. Honest, fair, timely and transparent policies
+                           customer-centric. Honest, fair, timely and transparent policies.
                         </p>
                      </div>
                   </div>
@@ -169,7 +170,7 @@ const Home = () => {
                <div class="row g-0 flickit-row">
                   <div class="trans-list-col-left col-md-6">
                      <div class="flickit-service-list  left-area">
-                        <div class="heading-sm">With <span>Traditional Insurers</span>, you get</div>
+                        <div class="heading-sm">With <span>Traditional Insurers</span>, you get :</div>
                         <ul class="flickit-list list-unstyled">
                            <li>Lengthy applications</li>
                            <li>Long and Lengthy claims Processing</li>
@@ -179,7 +180,7 @@ const Home = () => {
                   </div>
                   <div class="trans-list-col-right col-md-6 d-flex ">
                      <div class="flickit-service-list   right-area">
-                        <div class="heading-sm">With Flickit, you get</div>
+                        <div class="heading-sm">With Flickit, you get :</div>
                         <ul class="flickit-list list-unstyled">
                            <li>100% Digital</li>
                            <li>Get insured in minutes</li>
@@ -219,9 +220,9 @@ const Home = () => {
             <div class="container-fluid container-xs">
                <div class="row  flickit-row">
                   <div class="col-md-12">
-                     <div class="heading-sm">Insurance Designed for <span>Today’s Ecosystems</span>.
+                     <div class="heading-sm">Insurance Designed for <span>Today’s Ecosystems.</span>
                         We’re
-                        Building Insurance fit for the future
+                        Building Insurance fit for the future.
                      </div>
                      <form class="">
                         <div class="custom-input-group input-group mb-3">
@@ -249,8 +250,8 @@ const Home = () => {
                   </div>
                   <div class="col-md-6 col-lg-7 col-xl-6">
                      <div class="flickit-content">
-                        <div class="heading-sm">Download our Latest App Now for <span>New Experience</span></div>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consect</p>
+                        <div class="heading-sm">Download our Latest App Now for <span>New Experience.</span></div>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consect.</p>
                         <div class="app-buttons">
                            <button type="button" class="btn p-0"><img alt="google-play-store" src={require('../static/img/playstore.png').default} /></button>
                            <button type="button" class="btn p-0"><img alt="app-store" src={require('../static/img/app-store.png').default} /></button>
